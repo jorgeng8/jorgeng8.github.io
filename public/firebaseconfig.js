93ab8e09fec833d7e7da1422c666045c0f830b4c
jorgeng8 <jorgengpsm@gmail.com>
2021-01-04 14:09:28 -0400

    comentario

---
+++ public/firebaseconfig.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyD5xA6HlNOLHv_8liMbvq_ExkjNpe3xvkc",
+    authDomain: "fir-proyecto-e2162.firebaseapp.com",
+    databaseURL: "https://fir-proyecto-e2162.firebaseio.com",
+    projectId: "fir-proyecto-e2162",
+    storageBucket: "fir-proyecto-e2162.appspot.com",
+    messagingSenderId: "347239155791",
+    appId: "1:347239155791:web:cbe90b1b326b78313ec23a"
+  };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
